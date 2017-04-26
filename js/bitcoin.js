a1f5a822c3110ee3ca81f20ce0fc68c3c567af68
khangtruong <khang.truong@outlook.fr>
2017-04-26 17:18:07 +0200

    WS added

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -1,10 +1,24 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
-};
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockcypher.com/v1/btc/main",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_info_number').append(data.name);
+      $('#bitcoin_block_number').append(data.height);
+      $('#bitcoin_mining_number').append(data.time);
+      $('#bitcoin_peer_number').append(data.peer_count);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_info_number').append(err+" N/A");
+      $('#bitcoin_block_number').append(err+" N/A");
+      $('#bitcoin_mining_number').append(err+" N/A");
+      $('#bitcoin_peer_number').append(err+" N/A");
+		}
+	});
+});
